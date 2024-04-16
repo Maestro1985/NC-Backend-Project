69743ecf4d2b5678cf3e93839f8907af71d3685c
Maestro1985 <ortumassimo@hotmail.com>
2024-04-16 10:25:28 +0100

    completed task 4

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -23,6 +23,17 @@ response.body.topics.forEach((topic) => {
 })
 })
 })
+
+test('GET:404 sends an appropriate status and error message when given a non-existant api', () => {
+    return request(app)
+      .get("/api/top")
+      .expect(404)
+      .then(response => {
+
+            
+        expect(response.body.msg).toBe('Api does not exist');
+      });
+  });
 })
 
 describe('API Endpoints', () => {
@@ -32,9 +43,53 @@ describe('API Endpoints', () => {
         .get('/api')
         .expect(200)
         .then(({body}) => {
-            console.log(body)
+            
           expect(body).toEqual(endpoints);
         });
     })
     
+})
+
+describe('/api/articles/:article_id endpoint', ()=>{
+
+
+test('GET: 200 responds with a single id of articles',()=>{
+    
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response) => {
+        expect(response.body.article.author).toBe("butter_bridge")
+        expect(response.body.article.title).toBe("Living in the shadow of a great man")
+        expect(response.body.article.article_id).toBe(1);
+        expect(response.body.article.body).toBe("I find this existence challenging")
+        expect(response.body.article.topic).toBe('mitch')
+        expect(typeof response.body.article.created_at).toBe('string')
+        expect(response.body.article.votes).toBe(100)
+        expect(response.body.article.article_img_url).toBe("https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700")
+        
+})
+})
+test('GET:404 sends an appropriate status and error message when given a valid but non-existent id', () => {
+  return request(app)
+    .get('/api/articles/100')
+    .expect(404)
+    .then((response) => {
+      expect(response.body.msg).toBe('Article does not exist');
+    });
+});
+
+test('GET:400 sends an appropriate status and error message when given an invalid id', () => {
+  return request(app)
+    .get('/api/articles/not-a-article')
+    .expect(400)
+    .then((response) => {
+      expect(response.body.msg).toBe('Bad request');
+    });
+});
+
+
+
+
+
 })
--- models/models.js
+++ models/models.js
@@ -1,7 +1,14 @@
 const db=require('../db/connection')
 
 
-exports.selectTopics = () => {
+exports.selectTopics = (endpoint) => {
+
+    
+
+    if(endpoint !== '/api/topics'){
+
+        return Promise.reject({ status: 404, msg: 'Api does not exist' });
+    }
     
     return db.query('SELECT * FROM topics').then((result) => {
         
@@ -9,4 +16,17 @@ exports.selectTopics = () => {
     });
   };
 
- 
+  exports.selectArticleById = (article_id) => {
+    return db
+      .query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+      .then(({rows})=>{
+  
+        if(rows.length===0){
+
+          return Promise.reject({status:404, msg:'Article does not exist'})
+  
+        }
+        return rows[0]
+
+      })
+    }
